b772f28fb491f60c32c716507ef9e9c0822ec6ec
robeerob <rob@rob.ee>
2017-06-05 22:37:41 +0200

    Adding new artefact command feature
    Requested this myself in an issue, and decided to contribute. New
    command that queries a local .json file with manually compiled
    artefact data, and serves the corresponding artefact information.
    Includes following behavior/options:
     1. The user can query both with full artefact name and keywords
     2. If the keyword exists for several artfacts, the bot asks the
        user to try again with full artefact name
    
    This closes #21.

---
+++ discord/botReactions/artefact.go
@@ -0,0 +1,89 @@
+// TODO: Remaining huzzah.go causes runtime error
+
+package botReactions
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"bytes"
+	"fmt"
+	"strings"
+	"text/tabwriter"
+	"github.com/bwmarrin/discordgo"
+	"github.com/adayoung/ada-bot/settings"
+)
+
+type artie struct {
+	Trigger string
+}
+
+type Artefact struct {
+    ID           int    `json:"id"`
+    Title        string `json:"title"`
+    Description  string `json:"description"`
+    Cost         string `json:"cost"`
+    Tags         string `json:"tags"`
+}
+
+func (p Artefact) toString() string {
+    return toJson(p)
+}
+
+func toJson(p interface{}) string {
+    bytes, err := json.Marshal(p)
+    if err != nil {
+        fmt.Println(err.Error())
+        os.Exit(1)
+    }
+
+    return string(bytes)
+}
+
+func (p *artie) Help() string {
+	return "Lookup <artefact> and get details."
+}
+
+func (p *artie) HelpDetail() string {
+	return p.Help()
+}
+
+func (p *artie) Reaction(m *discordgo.Message, a *discordgo.Member, mType string) Reaction {
+	w := &tabwriter.Writer{}
+	buf := &bytes.Buffer{}
+	keyword := m.Content[len(settings.Settings.Discord.BotPrefix)+len(p.Trigger)+1:]
+	artefacts := getArtefacts()
+  for _, p := range artefacts {
+    if strings.Contains(p.Tags, keyword) {
+			w.Init(buf, 0, 4, 0, ' ', 0)
+			fmt.Fprintf(w, "```\n")
+			fmt.Fprintf(w, "Title: \t%s\n", p.Title)
+			fmt.Fprintf(w, "Description: \t%s\n", p.Description)
+			fmt.Fprintf(w, "Cost: \t%s\n", p.Cost)
+			fmt.Fprintf(w, "\n```")
+			w.Flush()
+		}
+	}
+
+	out := buf.String()
+	return Reaction{Text: out}
+}
+
+func getArtefacts() []Artefact {
+  raw, err := ioutil.ReadFile("./utils/data/artefacts.json")
+  if err != nil {
+      fmt.Println(err.Error())
+      os.Exit(1)
+  }
+
+  var c []Artefact
+  json.Unmarshal(raw, &c)
+  return c
+}
+
+func init() {
+	_artie := &artie{
+		Trigger: "artefact",
+	}
+	addReaction(_artie.Trigger, "CREATE", _artie)
+}
